5e9e7e589c4ffcac0e070b423d03526ef4a3f763
KarlMarx-spec <55876912+KarlMarx-spec@users.noreply.github.com>
2020-07-31 17:13:09 +0300

    Delete 2.2 slon.py

--- Основы Python/2.2 slon.py
+++
@@ -1,7 +0,0 @@
-first1 = int(input());
-first2 = int(input());
-second1 = int(input());
-second2 = int(input());
-if (abs(second1-first1)== abs(second2-first2)):
-    print ("YES");
-else: print ("NO");

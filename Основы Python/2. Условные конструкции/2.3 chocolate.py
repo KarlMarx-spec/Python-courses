617fa69bfab07cfdced1c38540c7886e9e97066f
KarlMarx-spec <55876912+KarlMarx-spec@users.noreply.github.com>
2020-08-03 13:37:50 +0300

    Add files via upload

--- Основы Python/2. Условные конструкции/2.3 chocolate.py
+++ Основы Python/2. Условные конструкции/2.3 chocolate.py
@@ -1,7 +1,11 @@
-N=int(input());
-M=int(input());
-K=int(input());
-if ((K % N == 0) and (K // N < M)) or ((K % M == 0) and (K // M < N)):
-    print("YES");
+try:
+        N=int(input());
+        M=int(input());
+        K=int(input());
+except: 
+        print ("Данные введены некорректно")
 else:
-    print("NO");
+        if ((K % N == 0) and (K // N < M)) or ((K % M == 0) and (K // M < N)):
+                print("YES");
+        else:
+                print("NO");

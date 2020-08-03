5bdc91b2765850291e93f44d01c0d7ac590bec74
KarlMarx-spec <55876912+KarlMarx-spec@users.noreply.github.com>
2020-08-03 13:27:54 +0300

    Add files via upload

--- Основы Python/2. Условные конструкции/2.2 slon.py
+++ Основы Python/2. Условные конструкции/2.2 slon.py
@@ -1,7 +1,11 @@
-first1 = int(input());
-first2 = int(input());
-second1 = int(input());
-second2 = int(input());
-if (abs(second1-first1)== abs(second2-first2)):
-    print ("YES");
-else: print ("NO");
+try:
+        first1 = int(input());
+        first2 = int(input());
+        second1 = int(input());
+        second2 = int(input());
+except: 
+        print ("Данные введены некорректно");
+else:
+        if (abs(second1-first1)== abs(second2-first2)):
+                print ("YES");
+        else: print ("NO");
